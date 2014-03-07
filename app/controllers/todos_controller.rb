53311872b7f067ebbc315b9051cd4d188cfaed8f
Tony Dahlager <tonydahlager@gmail.com>
2014-03-06 20:29:55 -0600

    build new view

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -7,4 +7,19 @@ class TodosController < ApplicationController
 		@todo = Todo.new
 	end
 
+	def create
+		@todo = Todo.create(params[:todo].permit(:title, :description, :completed))
+		if @todo.save
+			render 'todos'
+		else
+			render 'new'
+		end
+	end
+
+	private 
+		def todo_params
+			params.require(:todo).permit(:title, :desctiption, :completed)
+		end
+
+
 end
